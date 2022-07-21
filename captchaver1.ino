9059aee6b7c0cd9f7762f78454b7103364519143
Mikołaj Saramonowicz <252964@student.pwr.edu.pl>
2022-07-21 21:36:27 +0200

    Update captchaver1.ino

--- captchaver1.ino
+++ captchaver1.ino
@@ -1,24 +1,39 @@
+//Zdefiniowanie liczby pinow i ekspanderow
 #define ileScalakow 2
 #define ilePinow ileScalakow * 8
+
+//Zdefiniowanie maksymalnej dlugosci kombinacji, ktora uzytkownik musi zapamietac
 #define DLUGOSC_KOMBINACJI 5
-#include <PCF8574.h>
+
+//Biblioteki ktore nalezy miec pobrane w Arduino IDE
+#include <PCF8574.h> //Do obslugi ekspanderow
 #include <Wire.h>   
 #include <LiquidCrystal_I2C.h> 
 
+//Czestotliwosci obslugiwane przez buzzer
 int tableOfFrequency[16]= {200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2600, 2800, 3000, 3200, 3400};
+
+//Utworzenie obiektow ekspanderow
 PCF8574 expander1;
 PCF8574 expander2;
+
+//Przypisanie odpowiednich pinow procesora wlasciwym wejsciom rejestru przesuwnego
 int SER=8; 
 int RCLK=9;
 int SRCLK=10; 
+
 int rejestr[ilePinow];
-LiquidCrystal_I2C lcd(0x3F,16,2);  // Ustawienie adresu ukladu na 0x27
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  // Ustawienie adresu konwertera I2C, odczytanego za pomoca zewnetrzengo programu
 int kombinacja[DLUGOSC_KOMBINACJI];
-int rekord = 0;
+int rekord = 0; //Zmienna przechowujaca najlepszy otrzymany wynik
+
+//Zmienne pomocnicze
 volatile int number =-1;
 volatile int lastNumber =-1;
 int pom;
 
+//Funkcje pomocnicze do realizacji przerwan z wykorzystaniem ekspanderow i przyciskow
 
 void onPin0()
 {
@@ -88,33 +103,39 @@ void onPin15()
   number=7;
   lastNumber =0;
 }
- 
 
+//Po wykryciu przerwania funkcja sprawdza ktory pin je wywolal
 void onInterrupt()
 {
   expander1.checkForInterrupt();
   expander2.checkForInterrupt();
 }
 
+//Inicjalizacja programu
 void setup(){
-  Serial.begin(9600);
-
+  Serial.begin(9600); //Ustawienie predkosci przesylania danych 
+  
+  //Ustawienie ekspanderom wlasciwych adresow
   expander1.begin(0x38);
   expander2.begin(0x39);
- for(int i=0; i<8; i++){
-  expander1.pinMode(i, INPUT);
-  expander1.pullUp(i);
- }
+  
+  //Ustawienie odpowiednich stanow pinow ekspanderow - wejscie input pullup
+  for(int i=0; i<8; i++){
+    expander1.pinMode(i, INPUT);
+    expander1.pullUp(i);
+  }
 
- for(int i=0; i<3; i++){
-  expander2.pinMode(i, INPUT);
-  expander2.pullUp(i);
- }
+  for(int i=0; i<3; i++){
+   expander2.pinMode(i, INPUT);
+   expander2.pullUp(i);
+  }
 
 
   pinMode(2, INPUT);
   digitalWrite(2, HIGH);
-
+  
+  
+  //Wlaczenie obslugi przerwan
   expander1.enableInterrupt(2, onInterrupt);
 
   expander1.attachInterrupt(0, onPin0, FALLING);
@@ -125,34 +146,47 @@ void setup(){
   expander2.attachInterrupt(1, onPin9, FALLING);
   expander2.attachInterrupt(2, onPin10, FALLING);
   expander2.attachInterrupt(3, onPin11, FALLING);
- expander2.attachInterrupt(4, onPin12, FALLING);
- expander2.attachInterrupt(5, onPin13, FALLING);
- expander2.attachInterrupt(6, onPin14, FALLING);
- expander2.attachInterrupt(7, onPin15, FALLING);
+  expander2.attachInterrupt(4, onPin12, FALLING);
+  expander2.attachInterrupt(5, onPin13, FALLING);
+  expander2.attachInterrupt(6, onPin14, FALLING);
+  expander2.attachInterrupt(7, onPin15, FALLING);
   
   pinMode(SER, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(SRCLK, OUTPUT);
+  
+  //Reset stanu pinow rejestrow przesuwnych
   czyscRejestr();
   zapiszRejestr();
+  
+  //Inicjalizacja wyswietlacza
   lcd.init();  
   lcd.backlight(); // zalaczenie podwietlenia 
   lcd.setCursor(0,0); 
   lcd.print("LCD & I2C");
+  
   delay(500);
   lcd.setCursor(0,1);
   lcd.print("Rekord sesji: ");
-  lcd.print(rekord);  
+  lcd.print(rekord);
+  
+  //Inicjalizacja generatora liczb losowych na podstawie szumu na wybranym pinie
   randomSeed(analogRead(0));
+  
+  //Zapisanie wylosowanej kombinacji
   for(int i = 0; i<DLUGOSC_KOMBINACJI; i++)
   {
     kombinacja[i] = random(0,12);
   }  
 }
+
+
+//Pomocnicze funkcje do obslugi rejestrow przesuwnych///////
 void czyscRejestr(){
   for(int i=0; i<ilePinow; i++)
     rejestr[i]=LOW;
 }
+
 void zapiszRejestr(){
   digitalWrite(RCLK, LOW); 
   for(int i=ilePinow-1; i>=0; i--){
@@ -162,19 +196,21 @@ void zapiszRejestr(){
   }
   digitalWrite(RCLK, HIGH); 
 }
+
 void ustawPin(int ktory, int wartosc){
   rejestr[ktory]=wartosc;
 }
 
+//Wyswietlenie fragmentu wylosowanej kombinacji
 void wyswietl_do_elementu(int element)
 {
   czyscRejestr();
   for(int i = 0; i < element+1; i++)
   {
-    tone(A3, tableOfFrequency[kombinacja[i]]);
+    tone(A3, tableOfFrequency[kombinacja[i]]); //efekty dzwiekowe buzzerem
     delay(100);
     noTone(A3);
-    ustawPin(kombinacja[i],HIGH);
+    ustawPin(kombinacja[i],HIGH); //Przelaczenie pinu
     zapiszRejestr();
     delay(800);
     ustawPin(kombinacja[i],LOW);
@@ -185,7 +221,7 @@ void wyswietl_do_elementu(int element)
 
 void loop(){
 
-  int a = -1; //ilosc poprawnych indeksow
+  int a = -1; //ilosc poprawnie odgadnietych indeksow
   czyscRejestr();
   for(int i = 0; i < DLUGOSC_KOMBINACJI; i++)
   {
@@ -195,7 +231,7 @@ void loop(){
     {
       if(lastNumber == 0)
       {
-        if(number == kombinacja[a+1])
+        if(number == kombinacja[a+1])//Poprawne wprowadzenie odpowiedzi
         {
           tone(A3, tableOfFrequency[kombinacja[a+1]]);
           delay(100);
@@ -210,7 +246,7 @@ void loop(){
           lastNumber = -1;
           delay(100);
         }
-        else
+        else //Niepoprawne wprowadzenie odpowiedzi
         {
              tone(A3, tableOfFrequency[0]);
              delay(200);
@@ -224,11 +260,12 @@ void loop(){
              lcd.setCursor(0,1);
              lcd.print("Rekord sesji: ");
              lcd.print(rekord);
-             while(1){delay(10000);}      
+             while(1){delay(10000);} //Oczekiwanie na reset      
         }
       }
       delay(100);
     }
+    //Aktualizacja wyniku
     tone(A3, tableOfFrequency[7]);
     delay(200);
     noTone(A3);
@@ -248,5 +285,5 @@ void loop(){
   lcd.setCursor(0,1);
   lcd.print("Rekord sesji: ");
   lcd.print(rekord);
-  while(1){delay(10000);}           
+  while(1){delay(10000);} //Oczekiwanie na reset      
 }
